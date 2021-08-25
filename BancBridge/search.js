881afcbe16bfae2b42bad993ea714e4d6efe39ca
Ved Panse <vedkpanse@gmail.com>
2021-08-25 08:27:36 +0530

    Added ids for search function

--- BancBridge/search.js
+++ BancBridge/search.js
@@ -5,3 +5,12 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+
+let query
+
+document.querySelector("#search").addEventListener("keydown", function(e) {
+    if (e.keyCode === 13) {
+        query = document.querySelector("#search").value
+        alert(query)
+    }
+})
