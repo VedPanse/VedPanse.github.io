8bdbf2278ee97fb48ee0991b650d43effde9859b
Ved Panse <vedkpanse@gmail.com>
2021-08-30 09:33:31 +0530

    New Design

---
+++ New Design/js/functions.js
@@ -0,0 +1,11 @@
+function amazon() {
+    window.open("https://github.com/VedPanse/Python_Advanced/blob/master/AmazonPriceTracker/main.py", "_blank")
+}
+
+function iss() {
+    window.open("https://github.com/VedPanse/Python_Advanced/blob/master/ISSOverHeadNotifier/main.py", "_blank")
+}
+
+function workout() {
+    window.open("https://github.com/VedPanse/Python_Advanced/blob/master/Workout%20Tracking/main.py", "_blank")
+}
