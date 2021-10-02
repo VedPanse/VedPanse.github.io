f774fd89a173e875f33309a65e355011e0a0e711
Ved Panse <vedkpanse@gmail.com>
2021-10-02 13:40:34 +0530

    Changed black background Color from black to #1717

--- social/theme.js
+++ social/theme.js
@@ -1,5 +1,5 @@
 function themeChange() {
-    var dark = document.querySelector("body").style.backgroundColor === "black";
+    var dark = document.querySelector("body").style.backgroundColor === "#171717";
 
     if (!dark) {
         changeToDark();
@@ -10,7 +10,7 @@ function themeChange() {
 }
 
 function changeToDark() {
-    document.querySelector("body").style.backgroundColor = "black";
+    document.querySelector("body").style.backgroundColor = "#171717";
     document.querySelectorAll("h1").forEach((item) => {
         item.style.color = "white";
     });
