8159cd478825f99d1575c3e454a2cf8c498ab659
Ved Panse <vedkpanse@gmail.com>
2022-01-28 21:43:32 +0530

    Fixed navbar "fixed" brownie width change scroll

--- js/semi-navbar.js
+++ js/semi-navbar.js
@@ -4,6 +4,8 @@ document.addEventListener("scroll", function() {
         nav.style.top = "inherit";
         nav.style.backgroundColor = "rgba(255, 255, 255)";
         nav.style.backdropFilter = "none";
+        nav.style.width = "85%";
+        nav.style.left = "7.5%";
 
     } else {
         nav.style.position = "fixed";
@@ -12,5 +14,7 @@ document.addEventListener("scroll", function() {
         nav.style.width = "85%";
         nav.style.backgroundColor = "rgba(255, 255, 255, 0.72)";
         nav.style.backdropFilter = "saturate(180%) blur(20px)";
+        nav.style.width = "100%";
+        nav.style.left = "0%";
     }
 });
