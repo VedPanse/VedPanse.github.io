f846a24e7ed51cdf1544999babc1c512e6b73e00
Ved Panse <vedkpanse@gmail.com>
2023-06-07 23:07:30 +0530

    adjusted link

--- blogs/navbar.js
+++ blogs/navbar.js
@@ -21,6 +21,7 @@ document.querySelector("#all-blogs").addEventListener("click", () => {
 });
 
 document.querySelector("#overlay-hide").addEventListener("click", () => {
+    alert("click")
     document.querySelector("#expanded-navbar").classList.toggle("hidden");
     document.querySelector("#overlay-hide").classList.toggle("hidden");
 });
