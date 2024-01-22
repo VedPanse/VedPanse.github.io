2c6b1d38f4219531a979200415a26c7ac476cf05
Ved Panse <vedkpanse@gmail.com>
2024-01-21 20:31:08 -0800

    Added copy link to social

--- social/navbar.js
+++ social/navbar.js
@@ -3,8 +3,11 @@ document.querySelector("#all-blogs").addEventListener("click", () => {
     document.querySelector("#overlay-hide").classList.toggle("hidden");
 });
 
-document.querySelector("#overlay-hide").addEventListener("click", () => {
-    alert("click")
-    document.querySelector("#expanded-navbar").classList.toggle("hidden");
-    document.querySelector("#overlay-hide").classList.toggle("hidden");
-});
+document.querySelector("#share").addEventListener("click", () => {
+    var text = window.location.href;
+    navigator.clipboard.writeText(text).then(function() {
+      alert('Link copied!');
+    }, function(err) {
+      alert('Error while copying link: ', err);
+    });
+})
