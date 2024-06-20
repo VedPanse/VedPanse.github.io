37b0a1949b30ba421e05318caae0424c8f9f26bf
Ved Panse <vedkpanse@gmail.com>
2024-06-20 17:05:34 +0530

    fixed header responsiveness

--- js/navbar.js
+++ js/navbar.js
@@ -14,4 +14,41 @@ document.addEventListener("DOMContentLoaded", () => {
             menuIcon.innerHTML = hamMark;
         }
     });
+
+    // SMALL SCREEN SIZE
+    const windowWidth = window.innerWidth;
+
+    // Show header instead of navbar-nav
+    const hamburgerMenu = document.querySelectorAll("span.navbar-toggler-icon")[0];
+    hamburgerMenu.addEventListener("click", () => {
+        header.classList.toggle("show");
+    });
+
+    if (windowWidth <= 1000) {
+        // Add download resume button to div#header
+        const resumeButton = document.createElement("button");
+        resumeButton.innerHTML = "Download Resume";
+
+        const classList = ["btn", "resume"];
+
+        classList.forEach((className) => {
+            resumeButton.classList.add(className);
+        });
+
+        const link = "https://docs.google.com/document/d/1EMRoPJlL2s6foPICjBWRBImDrlklFWoK/edit#heading=h.gjdgxs";
+
+        resumeButton.addEventListener("click", () => {
+            window.open(link, '_blank')
+        })
+
+        header.appendChild(resumeButton);
+
+        // When a header link is clicked, close header
+        const allHeaderLinks = document.querySelectorAll("div#header a");
+        allHeaderLinks.forEach((item) => {
+            item.addEventListener("click", function () {
+                header.classList.toggle("show");
+            })
+        });
+    }
 });
