f82c38c64f8d718ed8d14024a234062ad349a67d
VedPanse <panse.ved@yandex.com>
2024-07-09 21:17:22 +0530

    removed comment

--- test/vite.config.js
+++ test/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/test/', // This should match your repository name if it's a project page
+  base: '/test/',
   plugins: [react()],
 });
