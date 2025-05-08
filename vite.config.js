53a793c58deadebbce88a30ffaa296b6d9b21b6d
VedPanse <panse.ved@yandex.com>
2025-05-07 22:01:54 -0700

    init

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/', // Keep this if you're using a custom domain like vedpanse.com
-  plugins: [react()]
-})
